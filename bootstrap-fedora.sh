a0a326e5229bc30b5a372ae12541bcf90fcb7931
Øyvind Ødegård <whirlwin@localhost.localdomain>
2015-06-06 14:59:27 +0200

    Replace yum with dnf.
    - Reorder installs
    - Add sudo check to the start of the script

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -1,8 +1,17 @@
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo"
+    exit 1
+fi
+
 # Make sure the system is up to date
-yum update -y
+dnf update -y
+
+# Configure Vim
+dnf install -y vim
+wget https://raw.github.com/whirlwin/init/master/editors/.vimrc -O ~/.vimrc
 
 # Configure git
-yum install -y git
+dnf install -y git
 wget https://raw.githubusercontent.com/whirlwin/init/master/.gitignore_global -O ~/.gitignore_global
 git config core.editor vim
 git config --global push.default simple
@@ -11,32 +20,28 @@ git config --global push.default simple
 # Configure Google Chrome
 GOOGLE_CHROME="google-chrome.rpm"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -O $GOOGLE_CHROME
-yum install -y $GOOGLE_CHROME
+dnf install -y $GOOGLE_CHROME
 rm $GOOGLE_CHROME
 
 # Misc
-yum install -y alacarte maven nodejs npm mongodb mongodb-server
+dnf install -y alacarte maven nodejs npm mongodb mongodb-server
 wget https://raw.github.com/whirlwin/init/master/.profile -O ~/.profile
 mkdir ~/Projects
 
 # Configure Guake
-yum install -y guake
+dnf install -y guake
 gconftool-2 --set /apps/guake/general/use_vte_titles --type boolean false
 
 # Configure zsh and oh-my-zsh
-yum install -y zsh
+dnf install -y zsh
 curl -L http://install.ohmyz.sh | sh
 chsh -s /usr/bin/zsh whirlwin
 cd ~/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
 # Configure xclip
-yum install -y xclip
+dnf install -y xclip
 echo "alias xclip=set selection -c" >> ~/.zshrc
-
-# Configure Vim
-yum install -y vim
-wget https://raw.github.com/whirlwin/init/master/editors/.vimrc -O ~/.vimrc
 
 # Log out in order to propagate changes
 gnome-session-quit --no-prompt
