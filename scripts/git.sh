cb8cc8e9fcc0523de7cd78bc7aacbe53c7fabdb9
whirlwin <whirlwin90@gmail.com>
2016-10-16 17:15:14 +0200

    Output public key.

--- scripts/git.sh
+++ scripts/git.sh
@@ -10,3 +10,6 @@ git config --global core.excludesfile ~/.gitignore_global
 git config --global user.name whirlwin
 git config --global user.email $1
 git config --global core.editor vim
+
+echo "SSH key: $(cat ~/.ssh/id_rsa.pub)"
+
