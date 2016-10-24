69dccd00738334411dbdee745b56ed363486a8dd
Øyvind <oyvind.odegaard@sparebank1.no>
2016-10-24 16:32:29 +0200

    Read name for git from cli.

--- scripts/git.sh
+++ scripts/git.sh
@@ -2,15 +2,19 @@
 
 ssh-keygen
 
+echo 'Please type in your name: '
+read NAME
+
 echo 'Please type in your email address: '
 read EMAIL
+
 
 # Configure global gitignore
 SCRIPT_DIRECTORY=$(dirname $0)
 cp -f "$SCRIPT_DIRECTORY/../.gitignore_global" ~/.gitignore_global
 
 git config --global core.excludesfile ~/.gitignore_global
-git config --global user.name whirlwin
+git config --global user.name $NAME
 git config --global user.email $EMAIL
 git config --global core.editor vim
 
