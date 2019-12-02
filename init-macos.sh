42ef135c5308010ffd0391b703399e2a0024a73c
Øyvind Ødegård <oyvind.odegaard@dfind.no>
2019-12-02 09:39:15 +0100

    Create git directories in init script for macOS

--- init-macos.sh
+++ init-macos.sh
@@ -4,4 +4,8 @@ set -e
 
 brew install neovim
 cp -r .nvim ~
-echo "[INFO]: Successfully copied neovim config ✅"
+echo "[INFO]: Copied neovim config ✅"
+
+mkdir -p ~/git/open-source
+echo "[INFO]: Created directories: ~/git ~git/open-source ✅"
+
