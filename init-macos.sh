554f7b0159ea8377f4c34b3fb9aa5913b570ac26
Øyvind Ødegård <oyvodega@mbp15-rafft1.schibsted.no>
2019-12-10 15:50:49 +0100

    Install sdkman
    - Install zsh-syntax-highlighting

--- init-macos.sh
+++ init-macos.sh
@@ -10,6 +10,7 @@ git config --global user.name "Øyvind Ødegård"
 
 echo "[INFO]: Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install zsh-syntax-highlighting
 
 echo "[INFO]: Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -17,6 +18,9 @@ echo "[INFO]: Installing Homebrew"
 brew install neovim
 cp -r .nvim ~
 echo "[INFO]: Copied neovim config ✅"
+
+echo "[INFO]: Installing sdkman"
+curl -s "https://get.sdkman.io" | bash
 
 mkdir -p ~/git/open-source
 echo "[INFO]: Created directories: ~/git ~git/open-source ✅"
