45842b2a5cb1319812623e390bb8357dd90c2021
Ødegård <oyvodega@mbp15-rafft1.schibsted.no>
2019-12-10 11:09:33 +0100

    Add profile picture

--- init-macos.sh
+++ init-macos.sh
@@ -2,6 +2,9 @@ echo "[INFO]: Installing stuff...⚙️️"
 
 set -e
 
+echo "[INFO]: Installing Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew install neovim
 cp -r .nvim ~
 echo "[INFO]: Copied neovim config ✅"
