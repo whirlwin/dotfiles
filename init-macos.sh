e1a7a6fd2d184a036352017a67a4d99420367d19
Ødegård <oyvodega@mbp15-rafft1.schibsted.no>
2019-12-10 11:13:46 +0100

    Add ssh-keygen to init script

--- init-macos.sh
+++ init-macos.sh
@@ -2,6 +2,9 @@ echo "[INFO]: Installing stuff...⚙️️"
 
 set -e
 
+echo "[INFO]: Setting up SSH keypair"
+ssh-keygen -o -a 100 -t ed25519
+
 echo "[INFO]: Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
