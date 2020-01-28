3d3c6ea0657e9b277c1b2532085cf41fc9fe34cb
Øyvind Ødegård <oyvind.odegard@schibsted.no>
2020-01-28 15:35:20 +0100

    Install dive with Homebrew

--- init-macos.sh
+++ init-macos.sh
@@ -40,6 +40,7 @@ cp .profile ~/
 echo "[INFO]: Setting up misc"
 brew install watch
 brew install htop
+brew install dive
 
 echo "[INFO]: Downloading iterm2 zip. Install it manually"
 (cd ~/Downloads && curl -O https://iterm2.com/downloads/stable/latest)
