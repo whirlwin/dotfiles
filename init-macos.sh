ef731a2538c29f62b79996384648c6e7d3895bc8
Øyvind Ødegård <oyvind.odegard@schibsted.no>
2020-02-13 21:40:15 +0100

    Add brew install for kind and helm

--- init-macos.sh
+++ init-macos.sh
@@ -43,6 +43,8 @@ brew install htop
 brew install dive
 brew install prometheus
 brew install wget
+brew install kind
+brew install helm
 
 echo "[INFO]: Downloading iterm2 zip. Install it manually"
 (cd ~/Downloads && curl -O https://iterm2.com/downloads/stable/latest)
