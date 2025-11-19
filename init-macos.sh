e134cc6b6db83a704d1c8ea08316946ff4492ca2
Øyvind Ødegård <whirlwin@users.noreply.github.com>
2025-11-19 21:23:13 +0100

    Remove commented flycut installation line
    Removed commented line for flycut installation.

--- init-macos.sh
+++ init-macos.sh
@@ -66,7 +66,6 @@ brew install autojump
 brew install fonts-powerline
 brew install font-hack-nerd-font
 brew install rust
-#brew install flycut
 brew install starship
 brew install rectangle
 brew install ripgrep
